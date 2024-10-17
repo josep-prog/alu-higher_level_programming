b688e7ae4b3c21ef81da4deccdda788e0510f4da
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-17 03:13:53 -0700

    eleventh activity

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-def delete_at(my_list=[], idx=0):
-    """function that deletes item at specific position in a list
 
-    If idx is negative or out of range return the same list
-    You are not allowed to use pop()
-    You are not allowed to import any module
+def delete_at(my_list=[], idx=0):
+    """Function that deletes item at specific position in a list.
+
+    If idx is negative or out of range, return the same list.
+    You are not allowed to use pop().
+    You are not allowed to import any module.
     """
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
@@ -13,3 +14,4 @@ def delete_at(my_list=[], idx=0):
     del my_list[idx]
 
     return my_list
+
