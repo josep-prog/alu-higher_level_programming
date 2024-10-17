49ea5c6a1dc331faf8ab960721bc4869a5ad990a
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-16 23:42:42 -0700

    seventh task

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Author - Joseph Nishimwe
 
-combinations = []
-for digit1 in range(10):
-    for digit2 in range(digit1 + 1, 10):
-        combinations.append("{:02}{:02}".format(digit1, digit2))
 
-# Join all combinations with ', ' and print
-print(", ".join(combinations))
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
