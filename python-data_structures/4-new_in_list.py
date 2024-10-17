cd15880a9627719b4bae956304169aab0f22771e
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-17 11:26:43 -0700

    fourth task

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -8,7 +8,7 @@ def new_in_list(my_list, idx, element):
     """
     if idx < 0 or idx >= len(my_list):
         return my_list[:]
-    
+
     # Create a copy of the original list
     new_list = my_list[:]
     new_list[idx] = element
