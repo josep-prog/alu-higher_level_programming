62ae6cd6f8b194284ae597a2480f15d5dadb15bb
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-17 02:11:11 -0700

    second task

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """Replace an element at a specific position in the list.
+
+    If idx is negative or out of range, return the original list.
+    """
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return my_list
+
