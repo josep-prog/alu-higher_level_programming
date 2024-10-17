40defaa25cdbf88145d22c5a8191ed0d00665274
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-17 11:32:03 -0700

    eighth task

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
+
 def multiple_returns(sentence):
-    """Function that returns a tuple with the length of a string and its first character."""
-    return (len(sentence), sentence[0] if sentence else None)
+    """function that returns tuple with the length of a str and its first,
+    character
+
+    If the sentence is empty, the first character should be equal to None
+    You are not allowed to import any module
+    """
+
+    sentence_length = len(sentence)
+
+    if sentence_length == 0:
+        new_tuple = (sentence_length, None)
+    else:
+        new_tuple = (sentence_length, sentence[0])
+
+    return new_tuple
