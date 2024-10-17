1f2479c448178ce4bb901294c7e6a86d2f0df478
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-17 03:28:48 -0700

    second task

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def replace_in_list(my_list, idx, element):
     """Replace an element at a specific position in the list.
 
@@ -8,4 +9,3 @@ def replace_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
         my_list[idx] = element
     return my_list
-
