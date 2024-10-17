a5f668eb5105c7079396219389b3ba03ac821422
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-17 01:30:29 -0700

    fourth task

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import hidden_4
+
+    # Use list comprehension to filter names and sort them
+    names = sorted([name for name in dir(hidden_4) if not name.startswith('__')])
+
+    # Print each name on a new line
+    for name in names:
+        print(name)
