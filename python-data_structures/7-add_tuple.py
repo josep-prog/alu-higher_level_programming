c06200830e51152b7513138a2cc300312849d3a6
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-17 02:35:04 -0700

    seventh task

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Function that adds two tuples."""
+    a1, a2 = (tuple_a + (0, 0))[:2]  # Default to 0 if missing
+    b1, b2 = (tuple_b + (0, 0))[:2]  # Default to 0 if missing
+    return (a1 + b1, a2 + b2)
