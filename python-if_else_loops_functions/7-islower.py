971e00dfd9c1a0778d5d323e915309500b561689
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-16 23:38:44 -0700

    seventh task

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 # Author - Joseph Nishimwe
 
-combinations = [f"{i}{j}" for i in range(10) for j in range(i + 1, 10)]
+combinations = []
+for digit1 in range(10):
+    for digit2 in range(digit1 + 1, 10):
+        combinations.append("{:02}{:02}".format(digit1, digit2))
+
+# Join all combinations with ', ' and print
 print(", ".join(combinations))
