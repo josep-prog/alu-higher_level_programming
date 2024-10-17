461899511f325ef4ed41ffaa05ca40057df8689f
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-17 02:38:30 -0700

    eighth task

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """Function that returns a tuple with the length of a string and its first character."""
+    return (len(sentence), sentence[0] if sentence else None)
