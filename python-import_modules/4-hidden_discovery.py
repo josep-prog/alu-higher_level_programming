490d54c4441b242172687d594f2c60f485756dea
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-17 01:32:35 -0700

    fourth task

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     import hidden_4
 
     # Use list comprehension to filter names and sort them
-    names = sorted([name for name in dir(hidden_4) if not name.startswith('__')])
+    names = sorted([
+        name for name in dir(hidden_4)
+        if not name.startswith('__')
+    ])
 
     # Print each name on a new line
     for name in names:
