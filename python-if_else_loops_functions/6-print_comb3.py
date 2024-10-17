23c24108319f829f6144f9040d16ad3d883e1841
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-16 23:31:30 -0700

    sith task

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,8 @@
 
 for digit1 in range(10):
     for digit2 in range(digit1 + 1, 10):
-        print("{}{}".format(digit1, digit2), end=", " if not (digit1 == 8 and digit2 == 9) else "\n")
+        combination = "{}{}".format(digit1, digit2)
+        if digit1 == 8 and digit2 == 9:
+            print(combination)
+        else:
+            print(combination, end=", ")
