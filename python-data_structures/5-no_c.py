f11fc1b61f982836c483eb0118aa87184a06f03b
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-17 02:26:42 -0700

    fifth task

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    """Remove all characters 'c' and 'C' from the string.
+
+    The function should return the new string.
+    You are not allowed to import any module.
+    You are not allowed to use str.replace().
+    """
+    new_str = ""
+    
+    for char in my_string:
+        if char not in ('c', 'C'):
+            new_str += char
+            
+    return new_str
