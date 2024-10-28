ceb53b2dbff9971e73df49849c2a85dfea655738
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-28 04:00:40 -0700

    zero task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -8,11 +8,11 @@ def safe_print_list(my_list=[], x=0):
     You have to use try: / except:
     You are not allowed to import any module
     You are not allowed to use len()
-    
+
     Args:
         my_list: list
         x: number of elements to print.
-    
+
     Returns: the real number of elements printed.
     """
     count = 0
