e46453bc5e8b9fccbb87474d97d7aa300599aef6
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-28 09:37:38 -0700

    second task

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""Defines a class Square"""
+
+class Square:
+    """Class that defines properties of a square."""
+
+    def __init__(self, size=0):
+        """Creates new instances of square."""
+        self.size = size  # Use the setter
+
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
