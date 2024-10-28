5ece273db539c33e35dc8275e31b462a7bd28cf6
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-28 08:36:20 -0700

    fifth task

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def raise_exception():
+    """Function that raises a type exception."""
+    if True:  # Always true, just to illustrate an alternative
+        raise TypeError
