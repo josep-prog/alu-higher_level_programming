8bcec2e2f6490f1ecb052218794cb13e25340fba
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-28 08:31:07 -0700

    fourth task

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,36 +2,29 @@
 
 
 def list_division(my_list_1, my_list_2, list_length):
-    """Divides element by element of two lists.
+    """function that divides element by element 2 lists.
 
     Args:
-        my_list_1: First list of elements.
-        my_list_2: Second list of elements.
-        list_length: Desired length of the result list.
+        my_list_1: list 1
+        my_list_2: list 2
+        list_length: length of list
 
-    Returns:
-        A new list containing the results of the divisions.
+    Returns: New list with all the divisions
     """
     new_list = []
 
-    for i in range(list_length):
-        quotient = 0  # Default value for quotient
-
-        if i >= len(my_list_1):
+    for item in range(list_length):
+        try:
+            quotient = my_list_1[item] / my_list_2[item]
+        except IndexError:
             print("out of range")
-        elif i >= len(my_list_2):
-            print("out of range")
-        else:
-            try:
-                if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
-                    quotient = my_list_1[i] / my_list_2[i]
-                else:
-                    raise TypeError
-            except ZeroDivisionError:
-                print("division by 0")
-            except TypeError:
-                print("wrong type")
-
-        new_list.append(quotient)
-
+            quotient = 0
+        except TypeError:
+            print("wrong type")
+            quotient = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            quotient = 0
+        finally:
+            new_list.append(quotient)
     return new_list
