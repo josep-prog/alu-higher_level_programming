1c141c576ce3b0fe433619174ef2849cdf9f88bf
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-29 00:42:20 -0700

    first task

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,32 +1,75 @@
 #!/usr/bin/python3
+"""Defines a class Rectangle"""
+
 
 class Rectangle:
-    """Defines a class Rectangle."""
-    
+    """
+    Class that defines properties of rectangle by: (based on 0-rectangle.py).
+
+    Attributes:
+        width (int): width of the rectangle.
+        height (int): height of the rectangle.
+    """
     def __init__(self, width=0, height=0):
-        self.width = width
+        """Creates new instances of Rectangle.
+
+        Args:
+            width (int, optional): width of rectangle. Defaults to 0.
+            height (int, optional): height of rectangle. Defaults to 0.
+        """
         self.height = height
+        self.width = width
 
     @property
     def width(self):
+        """Width retriver.
+
+        Returns:
+            int: the width of the rectangle.
+        """
         return self.__width
 
     @property
     def height(self):
+        """Height retriver.
+
+        Returns:
+            int: the height of the rectangle.
+        """
         return self.__height
 
     @width.setter
     def width(self, value):
-        self.__validate_dimension(value, "width")
-        self.__width = value
+        """Property setter for width of rectangle.
+
+        Args:
+            value (int): width of the rectangle.
+
+        Raises:
+            TypeError: if width is not an integer.
+            ValueError: if width is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @height.setter
     def height(self, value):
-        self.__validate_dimension(value, "height")
-        self.__height = value
+        """Property setter for height of recyangle.
 
-    def __validate_dimension(self, value, name):
+        Args:
+            value (int): height of the rectangle.
+
+        Raises:
+            TypeError: if height is not an integer.
+            ValueError: if height is less than 0.
+        """
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value < 0:
-            raise ValueError(f"{name} must be >= 0")
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
