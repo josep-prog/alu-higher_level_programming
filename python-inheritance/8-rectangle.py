a10e28453b4c538d8b1458a20480e46b52a7d5ae
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-06 00:51:04 -0800

    eighth task

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle based on 7-base_geometry.py."""
+"""Defines a class Rectangle based on 7-base_geometry.py.
+
+Attributes:
+    width (int): width of the rectangle.
+    height (int): height of the rectangle.
+"""
+
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle."""
+    """Class Rectangle"""
 
     def __init__(self, width, height):
         """Creates new instances of Rectangle.
@@ -14,7 +20,7 @@ class Rectangle(BaseGeometry):
             width (int): width of rectangle.
             height (int): height of rectangle.
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
