470a06423747fac0b1498aa6cf486f0387d66b15
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-06 00:37:57 -0800

    fifth task

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-"""Defines a function inherits_from()"""
+"""Defines an empty class BaseGeometry"""
 
 
-def inherits_from(obj, a_class):
-    """Returns True if the object is an instance of a class that inherited,
-    (directly or indirectly) from the specified class; otherwise False.
-
-    Args:
-        obj: object to check type.
-        a_class: type to check against.
-
-    Returns:
-        bool: True if obj is an instance of a subclass of a_class, else False.
+class BaseGeometry:
+    """Empty class
     """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    pass
