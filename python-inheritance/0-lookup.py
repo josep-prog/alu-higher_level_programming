a62d1e642648b8434afb62b93d6bf464a0abe595
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-06 00:09:20 -0800

    zero task

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Returns a function lookup()"""
 
+
 def lookup(obj):
-    """Function that returns the list of available attributes and methods
-    of an object.
+    """Function that returns the list of available attributes and methods,
+    f an object
 
     Args:
         obj (class): object
@@ -11,4 +12,4 @@ def lookup(obj):
     Returns:
         list: list of available attributes and methods of an object
     """
-    return list(vars(obj).keys()) + [method for method in dir(obj) if callable(getattr(obj, method))]
+    return dir(obj)
