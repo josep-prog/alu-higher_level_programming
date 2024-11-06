da4b45aed5932b3a5e372f9fae8e7bf208839ed4
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-06 00:44:28 -0800

    sixth task

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -2,17 +2,13 @@
 """Defines a class BaseGeometry based on 5-base_geometry.py"""
 
 
-class AreaNotImplementedError(Exception):
-    """Custom exception for unimplemented area method."""
-    pass
-
-
 class BaseGeometry:
-    """Class BaseGeometry."""
+    """Class BaseGeometry.
+    """
     def area(self):
         """Area function.
 
         Raises:
-            AreaNotImplementedError: if area is not implemented.
+            Exception: if area is not implemented.
         """
-        raise AreaNotImplementedError("area() is not implemented")
+        raise Exception("area() is not implemented")
