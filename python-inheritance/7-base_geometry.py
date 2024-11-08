f0f7605d82a35a3e1fd72fe071acca3324350c9a
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-08 05:15:19 -0800

    7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+uhvbeijdskvniusahfwe ewijfewuhoiew
+wefwefhiewojfwpefwe
+wefwoeijfiweojfewf weofijwef
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <=0:
+            raise ValueError(f"{name} must be greater than 0")
