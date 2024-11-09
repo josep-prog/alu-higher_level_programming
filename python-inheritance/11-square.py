9fad495f844d0597c096b3a1ca9752265e3bccb0
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-08 21:06:39 -0800

    11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,29 +1,44 @@
 #!/usr/bin/python3
-"""Defines a class Square based on 9-rectangle.py."""
+"""Defines a class Square based on 9-rectangle.py.
+
+Attributes:
+    width (int): width of the rectangle.
+    height (int): height of the rectangle.
+"""
+
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Defines a class Square."""
+    """Defines a class Square.
+
+    Args:
+        Rectangle (Rectangle): rectangle
+    """
 
     def __init__(self, size):
         """Creates new instances of class Square.
 
         Args:
-            size (int): size of one side of the square.
+            size (int): size of 1 side of square.
         """
         self.integer_validator("size", size)
-        super().__init__(size, size)  # Initialize both width and height as 'size'
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Returns a string representation of the square.
+
+        Returns:
+            str: square.
+        """
+        return ("[Square] {}/{}".format(self.__size, self.__size))
 
     def area(self):
-        """Calculates the area of the square.
+        """Calculates the area of a square.
 
         Returns:
             int: the area of the square.
         """
-        return self.width ** 2  # Access inherited width
-
-    def __str__(self):
-        """Returns string representation of the square."""
-        return f"[Square] {self.width}/{self.height}"  # Access inherited width/height
+        return self.__size ** 2
