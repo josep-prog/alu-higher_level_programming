b203a4625834451c67fb3c8cf5ee63f08b506b7e
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-13 01:40:14 -0800

    3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+"""Module containing the function to_json_string"""
 import json
 
 
-class CustomJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        try:
-            return super().default(obj)
-        except TypeError:
-            return str(obj)  # Or other custom handling
-
 def to_json_string(my_obj):
-    return json.dumps(my_obj, cls=CustomJSONEncoder)
+    """Returns the JSON representation of an object(string).
+
+    Args:
+        my_obj (type): object to examine.
+
+    Returns:
+        str: JSON representation of object.
+    """
+    # print("type json.dumps(my_obj)--> {}".format(type(json.dumps(my_obj))))
+    # print("type my_obj--> {}".format(type(my_obj)))
+
+    # serializing json
+    return json.dumps(my_obj)
