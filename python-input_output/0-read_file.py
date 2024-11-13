8080027e38903bd4aa459df6cecae566b4f66663
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-13 01:02:27 -0800

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module containing the function read_file"""
+
+def read_file(filename=""):
+    """Reads a file and prints to stdout, line by line.
+
+    Args:
+        filename (str, optional): name of file to read. Defaults to "".
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            print(line, end='')
