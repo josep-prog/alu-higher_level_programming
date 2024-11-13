77cfab2c7607bdc5d88220d15acea1903d1175ce
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-13 01:36:46 -0800

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+import json
+
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        try:
+            return super().default(obj)
+        except TypeError:
+            return str(obj)  # Or other custom handling
+
+def to_json_string(my_obj):
+    return json.dumps(my_obj, cls=CustomJSONEncoder)
