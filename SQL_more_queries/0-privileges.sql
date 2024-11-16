0dcc6344df3cdc19ccf16409cc65822a7bb2700b
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-16 10:57:57 -0800

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,18 +1,4 @@
--- Create users if they do not already exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Grant SELECT and INSERT privileges to user_0d_2 on user_2_db
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
--- Ensure the privileges are applied
-FLUSH PRIVILEGES;
-
--- List the privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List the privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Show these users' privilages
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
