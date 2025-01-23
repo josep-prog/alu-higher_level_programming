2ccd625706f397d41e4832a71eb3fae81841c52a
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-01-23 01:35:34 -0800

    10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-
-function factorial(a) {
-  if (isNaN(a) || a <= 0) return 1; // Return 1 if NaN or non-positive number
-  return a * factorial(a - 1); // Recursive call for factorial
+function factorial (a) {
+  if (isNaN(a) || (a <= 0)) {
+    return 1;
+  } else {
+    // using recursive way of finding factorial of number
+    return a * factorial(a - 1);
+  }
 }
-
-console.log(factorial(Number(process.argv[2]))); // Print the result
+console.log(factorial(Number(process.argv[2])));
